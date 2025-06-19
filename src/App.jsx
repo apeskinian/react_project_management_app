f6820ebfbcd170747c452e0b90a53be8a0bb295c
Matthew Aitken <matt@itken.net>
2025-06-19 15:57:28 +0100

    Show selected project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-import ProjectsSideBar from "./components/ProjectsSideBar"
-import NewProject from "./components/NewProject";
-import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSideBar from "./components/ProjectsSideBar.jsx"
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
   const [ projectsState, setProjectsState ] = useState({
@@ -44,7 +45,18 @@ function App() {
     });
   }
 
-  let content;
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAdd}/>
@@ -54,7 +66,7 @@ function App() {
 
   return (
     <main className="h-dvh my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects} onSelectProject={handleSelectProject} />
       {content}
     </main>
   );
