793710f61826c901adee5d28230cbdaffa8696ed
Matthew Aitken <matt@itken.net>
2025-06-19 14:01:35 +0100

    Add fallback component
    Also create new Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import ProjectsSideBar from "./components/ProjectsSideBar"
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-dvh my-8 flex gap-8">
       <ProjectsSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
